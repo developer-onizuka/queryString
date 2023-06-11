df4c3ac0c5f3cef495aacbf13b7053cd6ccaf008
developer-onizuka <developer@example.com>
2023-06-11 04:41:14 +0000

    2023-06-10

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        QueryString q = new QueryString();
+        q.printString("https://www.example.com/?val1=11111&val2=22222&val3=33333");
+    }
+}
---
+++ QueryString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web;
+using System.Collections.Specialized;
+
+public class QueryString
+{
+    public void printString(string url)
+    {
+        //string url = "https://www.google.com/?val1=11111&val2=22222&val3=33333";
+        Uri uri = new Uri(url);
+        string queryString = uri.Query;
+        //Console.WriteLine(queryString);
+
+        NameValueCollection nvc = HttpUtility.ParseQueryString(queryString);
+        foreach(string key in nvc.Keys) {
+            Console.WriteLine(key + "=" + HttpUtility.UrlDecode(nvc[key]));
+        }
+        //nvc["val1"] = "12345";
+        //Console.WriteLine(nvc.ToString());
+    }
+}
+
